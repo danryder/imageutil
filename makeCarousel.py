68f78d1909f21ba35a587c045bb187c8e64d402f
Dan Ryder <dan.ryder@gmail.com>
2014-09-20 22:10:08 -0400

    implement EXIF info popup

--- makeCarousel.py
+++ makeCarousel.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
 
 import os
-import sys
+
+# tags to extract from image EXIF
+# and display in popup
+exif_tags = ['DateTimeOriginal',
+             'ISO',
+             'Aperture',
+             'ShutterSpeed',
+             'Flash',
+             'FocalLength',
+             'WhiteBalance',
+             'ExposureProgram',
+             'AutoFocus',
+             'Model',
+             'Lens']
+exif_args = ['-%s' % t for t in exif_tags]
 
 mobileJS = """\
     $( document ).on( "pageinit", "[data-role='page'].album-page", function() {
@@ -93,62 +107,55 @@ headHTML = """\
 </head>
 """
 
-bodyHTML = """\
-<body>
-<div data-role="page" %(idattr)s class="album-page" data-dom-cache="true" data-theme="a" %(prevattr)s %(nextattr)s >
-    <div data-role="header" data-position="fixed" data-fullscreen="true" data-id="hdr" data-tap-toggle="false">
-        <h1>%(heading)s</h1>
-        <a href="backlinks" data-ajax="false" data-direction="reverse" data-icon="delete" data-iconpos="notext" data-shadow="false" data-icon-shadow="false">Back</a>
-    </div>
+backlinkHTML = """\
+        <a href="%(backlink)s" data-ajax="false" data-direction="reverse" data-icon="delete" data-iconpos="notext" data-shadow="false" data-icon-shadow="false">Back</a>
+"""
+
+popupHTML = """\
     <div data-role="content">
         <div id="exif" class="exif ui-content" data-role="popup" data-position-to="window" data-tolerance="50,30,30,30" data-theme="d">
             <a href="#" data-rel="back" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext" class="ui-btn-right">Close</a>
-            <p>TODO: insert ISO, Aperture, Shutter, flash info, metering, autofocus, focal length etc stuff here...</p>
+            %(metaHTML)s
         </div>
     </div>
-    <div data-role="footer" data-position="fixed" data-fullscreen="true" data-id="ftr" data-tap-toggle="false">
-        <div data-role="controlgroup" class="control ui-btn-left" data-type="horizontal" data-mini="true">
-            <a href="#" class="prev" data-role="button" data-icon="arrow-l" data-iconpos="notext" data-theme="d">Previous</a>
-            <a href="#" class="next" data-role="button" data-icon="arrow-r" data-iconpos="notext" data-theme="d">Next</a>
-        </div>
-        <a href="#exif" data-rel="popup" class="exif-btn ui-btn-right" data-role="button" data-icon="info" data-iconpos="left" data-theme="d" data-mini="true">EXIF</a>
-    </div>
-</div><!-- /page -->
-</body>
+"""
+popupButtonHTML = """\
+        <a href="#exif" data-rel="popup" data-transition="slidedown" class="exif-btn ui-btn-right" data-role="button" data-icon="info" data-iconpos="left" data-theme="d" data-mini="true">EXIF</a>
 """
 
 pageHTML = """\
 <div data-role="page" %(idattr)s class="album-page" data-dom-cache="true" data-theme="a" %(prevattr)s %(nextattr)s >
     <div data-role="header" data-position="fixed" data-fullscreen="true" data-id="hdr" data-tap-toggle="false">
         <h1>%(heading)s</h1>
-        <a href="backlinks" data-ajax="false" data-direction="reverse" data-icon="delete" data-iconpos="notext" data-shadow="false" data-icon-shadow="false">Back</a>
+        %(backlink)s
     </div>
-    <div data-role="content">
-        <div id="exif" class="exif ui-content" data-role="popup" data-position-to="window" data-tolerance="50,30,30,30" data-theme="d">
-            <a href="#" data-rel="back" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext" class="ui-btn-right">Close</a>
-            <p>TODO: insert ISO, Aperture, Shutter, flash info, metering, autofocus, focal length etc stuff here...</p>
-        </div>
-    </div>
+    %(popup)s
     <div data-role="footer" data-position="fixed" data-fullscreen="true" data-id="ftr" data-tap-toggle="false">
         <div data-role="controlgroup" class="control ui-btn-left" data-type="horizontal" data-mini="true">
             <a href="#" class="prev" data-role="button" data-icon="arrow-l" data-iconpos="notext" data-theme="d">Previous</a>
             <a href="#" class="next" data-role="button" data-icon="arrow-r" data-iconpos="notext" data-theme="d">Next</a>
         </div>
-        <a href="#exif" data-rel="popup" class="exif-btn ui-btn-right" data-role="button" data-icon="info" data-iconpos="left" data-theme="d" data-mini="true">EXIF</a>
+        %(popupbutton)s
     </div>
 </div><!-- /page %(idattr)s -->
 """
 
 import argparse
+import subprocess
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("imagefile",help="path to images to include in output HTML page", nargs="*")
+    parser.add_argument("imagefile", nargs="*", help="path to images to include in output HTML page")
+    parser.add_argument("-b", "--backlink", help="link to follow on exit")
+    parser.add_argument("-m", "--meta", help="retrieve and show exif metadata", action="store_true")
+    parser.add_argument("-t", "--title", help="page title")
     args = parser.parse_args()
 
     images = args.imagefile
     n = len(images)
 
     # create support files
+    # could inline since only making 1 HTML
     file("mobile.js", "w").write(mobileJS)
     file("mobile.css", "w").write(mobileCSS)
     outfile = "mobile.html"
@@ -165,24 +172,53 @@ if __name__ == "__main__":
             nextattr = 'data-next="%s"' % makePageId(i + 1)
         return (idattr, prevattr, nextattr)
 
-    # make mobile page
-    imagemap = ["/* Set the background image sources */"]
+    # make mobile-friendly page
+    imagemap = ["/* Set background image sources */"]
     imagemap.extend([ "#%s { background-image: url(%s); }" % (makePageId(i_ind), images[i_ind]) for i_ind in range(n) ])
     
+    if args.title:
+        title = args.title
+    else:
+        title = "%d images" % n
+
+    if args.backlink:
+        backlink = backlinkHTML % {'backlink': args.backlink}
+    else:
+        backlink = ''
+
+    meta = {}
+    if args.meta:
+        print "Gathering metadata..."
+        for i in range(n):
+            exif_cmd = ['exiftool'] + exif_args + ['-h', images[i]]
+            try:
+                meta[images[i]] = subprocess.check_output(exif_cmd)
+            except:
+                print "Error running: \"%s\"" % ' '.join(exif_cmd)
+                print "Ensure exiftool is installed"
+                print "Omitting EXIF metadata!"
+
+    print "Writing %s..." % outfile
     with file(outfile, 'w') as o:
         o.write("<!doctype html><html>")
-        o.write(headHTML % {'title': "%d images" % n,
+        o.write(headHTML % {'title': title,
                             'imagemap':'\n'.join(imagemap) })
         o.write("<body>")
         for i in range(n):
+            popup = popupbutton = ''
+            if meta:
+                popup = popupHTML % { 'metaHTML': meta[images[i]] }
+                popupbutton = popupButtonHTML
+            
             idattr, prevattr, nextattr = makeAttrs(i, n)
             heading = os.path.basename(images[i]) + ' (%d of %d)' % (i+1, n)
-            o.write(pageHTML % {'pagename':os.path.basename(images[i]),
-                                'heading':heading,
+            o.write(pageHTML % {'pagename': os.path.basename(images[i]),
+                                'backlink': backlink,
+                                'heading': heading,
+                                'popup': popup,
+                                'popupbutton': popupbutton,
                                 'idattr': idattr,
-                                'prevattr':prevattr,
-                                'nextattr':nextattr
+                                'prevattr': prevattr,
+                                'nextattr': nextattr
                                })
         o.write("</body></html>")
-    print "Wrote", outfile
-
